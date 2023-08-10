a02a09d1dc971031e29fc704491fbea15ee7c9af
Mohamed Taymour <mohamedtaymour1@gmail.com>
2023-08-10 12:06:39 +0300

    Added additional files into Account subsection

--- lib/AppScreens/Account dir/AccountScreen.dart
+++ lib/AppScreens/Account dir/AccountScreen.dart
@@ -6,13 +6,37 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    String profilePic = "Assets/Images/profile_test_pic.png";
     final double height = MediaQuery.of(context).size.height;
+    String Name = "Mohamed Taymour";
 
     return Scaffold(
       appBar: PreferredSize(
           preferredSize: Size.fromHeight(height * 0.075),
           child: AccountAppBar()),
-      body:Image.asset("C:/Users/moham/StudioProjects/TeethApp/lib/Assets/Icons/Tooth_Doc.jpg")
-    );
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.only(top:25)),
+            Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+            color: Colors.black,
+            border: Border.all(
+            color: Colors.black,
+            width: 2.5,
+            ),
+            borderRadius: BorderRadius.circular(100),
+            ),
+            child: CircleAvatar(
+            radius: 80,
+            foregroundImage: AssetImage(profilePic),
+            ),
+            ),
+          Padding(padding: EdgeInsets.only(top: 30)),
+          Expanded(child: Text(Name,style: TextStyle(fontSize: 36),textAlign: TextAlign.center,)),
+        ].map((e) => Padding(padding: EdgeInsets.only(left: 50),child: e,)).toList(),
+          ),
+        );
   }
 }
